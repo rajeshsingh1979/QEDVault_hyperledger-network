6bc1c09fe3b7d9ea912968d659e37f709ae4fdc4
jamilmoatasimbillah <jamil@qspear.com>
2022-05-16 23:19:56 +0530

    automate-join-channel stript bug fixed

--- network/automate/join-channel.sh
+++ network/automate/join-channel.sh
@@ -1,14 +1,14 @@
 channel_name=$1
 
-. ./scripts/set-channel $channel_name admin
+. ./scripts/set-channel.sh $channel_name admin
 osnadmin channel join --channelID $CHANNEL_ID --config-block ./channel-artifacts/${CHANNEL_ID}.block -o $ORDERER_ADDRESS --ca-file $ORDERER_CA --client-cert $ORDERER_ADMIN_TLS_SIGN_CERT --client-key $ORDERER_ADMIN_TLS_PRIVATE_KEY
 ./scripts/channel-join.sh $channel_name admin
 
-. ./scripts/set-channel $channel_name consumer
+. ./scripts/set-channel.sh $channel_name consumer
 osnadmin channel join --channelID $CHANNEL_ID --config-block ./channel-artifacts/${CHANNEL_ID}.block -o $ORDERER_ADDRESS --ca-file $ORDERER_CA --client-cert $ORDERER_ADMIN_TLS_SIGN_CERT --client-key $ORDERER_ADMIN_TLS_PRIVATE_KEY
 ./scripts/channel-join.sh $channel_name consumer
 
 
-. ./scripts/set-channel $channel_name manufacturer
+. ./scripts/set-channel.sh $channel_name manufacturer
 osnadmin channel join --channelID $CHANNEL_ID --config-block ./channel-artifacts/${CHANNEL_ID}.block -o $ORDERER_ADDRESS --ca-file $ORDERER_CA --client-cert $ORDERER_ADMIN_TLS_SIGN_CERT --client-key $ORDERER_ADMIN_TLS_PRIVATE_KEY
 ./scripts/channel-join.sh $channel_name manufacturer
